5c91eabcb196bac8d471bd27079e2338e29bd50d
Edison Trutwein <edisonjude90@gmail.com>
2023-05-19 23:05:53 +0530

    fix: update github url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/edisonjude90/edisonjude90.github.io/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/edisonjude90/edisonjude90.github.io/tree/main/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
